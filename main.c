3eb397a4cf36a37b0e54a69175a1a585ee35cce6
RogueEffect <therogueeffect@gmail.com>
2021-08-04 18:51:51 -0700

    readable syntax

--- main.c
+++ main.c
@@ -21,9 +21,9 @@ int main(int argc, char **argv) {
 char *reverse(char *str) {
     int len = strlen(str);
     for(int i = 0; i < len / 2; i++) {
-        char temp = *(str + i);
-        *(str + i) = *(str + len - 1 - i);
-        *(str + len - 1 - i) = temp;
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
     }
     return str;
 }
